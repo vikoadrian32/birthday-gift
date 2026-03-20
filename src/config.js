c4e4f3b7054240e44d2713047a2aacfcc25a3581
vikoadrian32 <vikosilaban1@gmail.com>
2026-03-20 14:04:56 +0700

    menambahkan background bunga

--- src/config.js
+++ src/config.js
@@ -63,9 +63,9 @@ export const config = {
   ],
 
   wishes: [
-  { id: 1, dari: "Bestie 1", teks: "Selamat ulang tahun Nessa! Semoga selalu bahagia dan sehat ya! 🎉" },
-  { id: 2, dari: "Bestie 2", teks: "Happy birthday! Semoga impianmu semua terwujud tahun ini 💖" },
-  { id: 3, dari: "Bestie 3", teks: "HBD Nessa! Makin cantik, makin sukses, makin bahagia ya! ✨" },
-  { id: 4, dari: "Bestie 4", teks: "Selamat ulang tahun! Semoga panjang umur dan selalu dalam lindungan Allah 🌸" },
+  { id: 1, dari: "Eli", teks: "Selamat ulang tahun Nessa! Semoga selalu bahagia dan sehat ya! 🎉" },
+  { id: 2, dari: "Tasya", teks: "Happy birthday! Semoga impianmu semua terwujud tahun ini 💖" },
+  { id: 3, dari: "Clara", teks: "HBD Nessa! Makin cantik, makin sukses, makin bahagia ya! ✨" },
+  { id: 4, dari: "Cia", teks: "Selamat ulang tahun! Semoga panjang umur dan selalu dalam lindungan Allah 🌸" },
 ],
 };
